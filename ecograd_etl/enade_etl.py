bb455199f4535c0d9f99c6791de129fa4364cf38
Marcus Carvalho <marcuswac@gmail.com>
2021-01-19 09:37:35 -0300

    fix

--- ecograd_etl/enade_etl.py
+++ ecograd_etl/enade_etl.py
@@ -29,26 +29,22 @@ def load_enade(zip_file_name, db_con, sql_table, sql_schema="public"):
     print(f"Loading {zip_file_name} to {sql_schema}.{sql_table}")
     csv_file = utils.open_file_from_zip(zip_file_name)
     cur_cols = utils.list_db_column_names(db_con, sql_table, sql_schema)
+    first = True
     for df in pd.read_csv(csv_file, delimiter = ";", chunksize=10000, low_memory=False):
         new_cols = df.columns.difference(cur_cols)
-        if not cur_cols.empty and not new_cols.empty:
+        if first and not cur_cols.empty and not new_cols.empty:
             print(f"New columns found: {new_cols}")
             utils.add_db_table_columns(new_cols, "text", db_con, sql_table, sql_schema)
-        df.to_sql(
-            sql_table, 
-            db_con,
-            sql_schema,
-            index=False,
-            if_exists='append',
-            dtype=sqlalchemy.types.Text
-        )
+        df.to_sql(sql_table, db_con, sql_schema, index=False, if_exists='append', 
+                  dtype=sqlalchemy.types.Text)
+        first = False
 
 def main(args):
     output_dir = "data"
-    db_url = utils.build_db_url("postgresql", os.getenv("POSTGRES_USER"), os.getenv("POSTGRES_PWD"),
-                                os.getenv("POSTGRES_HOST"), os.getenv("POSTGRES_PORT"),
-                                os.getenv("POSTGRES_DB"))
-    
+    db_url = utils.build_db_url(
+        "postgresql", os.getenv("POSTGRES_USER"), os.getenv("POSTGRES_PWD"),
+        os.getenv("POSTGRES_HOST"), os.getenv("POSTGRES_PORT"), os.getenv("POSTGRES_DB")
+    )
     sql_table = "microdados_enade"
     sql_schema = "enade"
     db_con = utils.connect_db(db_url)
