8017ba88c9a4eb0d9f761c6fd297f081603b1a0e
Marcus Carvalho <marcuswac@gmail.com>
2021-01-19 21:49:31 -0300

    change dict

--- ecograd_etl/indicadores_etl.py
+++ ecograd_etl/indicadores_etl.py
@@ -18,15 +18,26 @@ indicadores_enade = {
         "url": "https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2018/resultados_conceito_enade_2017.csv"
     },
     2016: {
-        "url": "https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2017/resultado_enade2016_portal_06_09_2017.csv",
-
+        "url": "https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2017/resultado_enade2016_portal_06_09_2017.csv"
     },
-    2015: "https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2017/conceito_enade_2015_portal_atualizado_03_10_2017.csv",
-    2014: "https://download.inep.gov.br/educacao_superior/enade/planilhas/2014/conceito_enade_2014_atualizado_em_04122017.csv",
-    2013: "https://download.inep.gov.br/educacao_superior/enade/planilhas/2013/conceito_enade_2013.csv",
-    2012: "https://download.inep.gov.br/educacao_superior/enade/planilhas/2012/conceito_enade_2012.csv",
-    2011: "https://download.inep.gov.br/educacao_superior/enade/planilhas/2011/tabela_enade_cpc_2011_retificado_08_02_13.csv",
-    2010: "https://download.inep.gov.br/educacao_superior/enade/planilhas/2010/tabela_enade_cpc_2010.csv"
+    2015: {
+        "url": "https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2017/conceito_enade_2015_portal_atualizado_03_10_2017.csv"
+    },
+    2014: {
+        "url": "https://download.inep.gov.br/educacao_superior/enade/planilhas/2014/conceito_enade_2014_atualizado_em_04122017.csv"
+    },
+    2013: {
+        "url": "https://download.inep.gov.br/educacao_superior/enade/planilhas/2013/conceito_enade_2013.csv"
+    },
+    2012: {
+        "url": "https://download.inep.gov.br/educacao_superior/enade/planilhas/2012/conceito_enade_2012.csv"
+    },
+    2011: {
+        "url": "https://download.inep.gov.br/educacao_superior/enade/planilhas/2011/tabela_enade_cpc_2011_retificado_08_02_13.csv",
+    },
+    2010: {
+        "https://download.inep.gov.br/educacao_superior/enade/planilhas/2010/tabela_enade_cpc_2010.csv"
+    }
 }
 
 def download_indicadores(url, output_file):
@@ -58,9 +69,9 @@ def main(args):
     db_con = utils.connect_db(db_url)
     utils.create_db_schema(db_con, sql_schema)
     utils.drop_db_table(db_con, sql_table, sql_schema)
-    for year, url in ind_enade_urls.items():
+    for year, item in indicadores_enade.items():
        csv_file = os.path.join(output_dir, f"conceito_enade_{year}.csv")
-       download_indicadores(url, csv_file)
+       download_indicadores(item["url"], csv_file)
        load_indicadores(csv_file, db_con, sql_table, sql_schema)
 
 if __name__ == '__main__':
