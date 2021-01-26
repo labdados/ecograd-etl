900ea308aa6a15b2a812d8f75c43804c533d70be
Marcus Carvalho <marcuswac@gmail.com>
2021-01-26 11:51:11 -0300

    municipios ETL

--- ecograd_etl/indicadores_dimensional.py
+++ ecograd_etl/indicadores_dimensional.py
@@ -6,14 +6,27 @@ import pandas as pd
 load_dotenv()
 
 def create_ies_table(db_con):
-    ies = pd.read_sql("SELECT DISTINCT ano, codigo_da_ies, nome_da_ies, sigla_da_ies FROM inep.indicadores", db_con)
+    # pega a entrada mais recente de nome e sigla da IES
     ies = (
-        ies.sort_values(by=["ano"], ascending=False)
-        .groupby(["codigo_da_ies"])
-        [["codigo_da_ies", "nome_da_ies", "sigla_da_ies"]]
+        pd.read_sql("""SELECT DISTINCT ano, codigo_da_ies AS id_ies, nome_da_ies AS nome_ies,
+                         sigla_da_ies AS sigla_ies FROM inep.indicadores
+                         ORDER BY ano DESC""", db_con)
+        .groupby(["id_ies"])
+        [["id_ies", "nome_ies", "sigla_ies"]]
         .first()
     )
     ies.to_sql("ies", db_con, "inep", index=False,  if_exists="replace")
+
+def create_municipio_table(db_con, sql_table="municipio", sql_schema="inep"):
+    # pega a entrada mais frequente de nome e uf de municipio para cada codigo
+    mun = (
+        pd.read_sql("""SELECT DISTINCT codigo_do_municipio AS id_municipio,
+                         UPPER(municipio_do_curso) AS nome_municipio, sigla_da_uf AS uf
+                         FROM inep.indicadores""", db_con)
+        .groupby(["id_municipio"])
+        .agg(lambda x:x.value_counts().index[0])
+    )
+    mun.to_sql(sql_table, db_con, sql_schema, index=False,  if_exists="replace")
 
 def etl_indicadores_dimensional():
     db_url = utils.build_db_url(
@@ -22,5 +35,6 @@ def etl_indicadores_dimensional():
     )
     db_con = utils.connect_db(db_url)
     create_ies_table(db_con)
+    create_municipio_table(db_con)
 
 etl_indicadores_dimensional()
