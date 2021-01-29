cea01d2be97ed076a98a9ce974c0cda88fc039c8
Marcus Carvalho <marcuswac@gmail.com>
2021-01-29 16:57:57 -0300

    idd datamart

--- ecograd_etl/inep/indicadores_datamart.py
+++ ecograd_etl/inep/indicadores_datamart.py
@@ -54,10 +54,13 @@ def create_fact_table(db_con, sql_table="avaliacao", sql_schema="inep"):
             SELECT cpc.ano, cpc.codigo_do_curso AS id_curso, cpc.codigo_da_area AS id_area,
                    cpc.codigo_da_ies AS id_ies, ca.id_categoria_administrativa,
                    cpc.codigo_do_municipio AS id_municipio, cpc.cpc_continuo, cpc.cpc_faixa,
-	               enade.conceito_enade_continuo, enade.conceito_enade_faixa
+	               enade.conceito_enade_continuo, enade.conceito_enade_faixa,
+                   idd.idd_continuo, idd.idd_faixa
 	        FROM inep.cpc cpc
 	        LEFT OUTER JOIN inep.enade enade
                 ON (cpc.ano = enade.ano and cpc.codigo_do_curso = enade.codigo_do_curso)
+            LEFT OUTER JOIN inep.idd idd
+                ON (cpc.ano = idd.ano and cpc.codigo_do_curso = idd.codigo_do_curso)
 	        LEFT OUTER JOIN inep.categoria_administrativa AS ca
                 ON cpc.categoria_administrativa = ca.categoria_administrativa""", db_con)
     )
